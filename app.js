02a560aaba5309d0271792489dadccdee0b54d48
Kartikey <kartikey.srivastava1271@gmail.com>
2023-05-16 04:59:27 +0530

    fundamentals of nodejs

---
+++ app.js
@@ -0,0 +1,7 @@
+module.exports = {
+    x: 10,
+    y: 20,
+    z: function () {
+        return "you will be successfull!Bhagwan k ghar der hai andher nhi"
+    }
+}
--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-console.log("sub");
+// const app = require('./app');
 
-var a = 20;
-var b = 30;
-var c = 40;
-console.log(a + b + c);
+// var a = 20;
+// var b = 30;
+// var c = 40;
+// console.log(a + b + c);
+// console.log(app.x);
+// console.log(app.z());
+
+// const arr = [2, 4, 7, 1, 3, 5, 3];
+// let array = arr.filter((item) => {
+//     return item === 3;
+// })
+// console.log(array);
+
+// const fs = require('fs').writeFileSync;
+// fs("helloworld2.txt", "You will do this anytime soon");
+// console.log("-->" + __dirname);
+// console.log(__filename);
+
+const http = require('http');
+http.createServer((req, res) => {
+    res.write("Jai shree Ram!");
+    res.end();
+}).listen(4500);
+
