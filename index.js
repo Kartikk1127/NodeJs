8ab40b9a2e0e804a42dec05dbd2aaf69fa3dea11
Kartikey <kartikey.srivastava1271@gmail.com>
2023-05-24 06:49:55 +0530

    working of node js

--- index.js
+++ index.js
@@ -1,61 +1,114 @@
-// // const http = require('http');
-// // const data = require('./data');
-// // http.createServer((request, response) => {
-// //     response.writeHead(200, { 'Content-Type': 'application\json' });
-// //     response.write(JSON.stringify(data));
-// //     response.end();
-// // }).listen(5000);
+// // // const http = require('http');
+// // // const data = require('./data');
+// // // http.createServer((request, response) => {
+// // //     response.writeHead(200, { 'Content-Type': 'application\json' });
+// // //     response.write(JSON.stringify(data));
+// // //     response.end();
+// // // }).listen(5000);
 
-// // console.log(process.argv[3])
+// // // console.log(process.argv[3])
+
+// // // const fs = require('fs');
+// // // const input = process.argv;
+
+// // // if (input[2] == 'add') {
+// // //     fs.writeFileSync(input[3], input[4]);
+// // // }
+// // // else if (input[2] == 'remove') {
+// // //     fs.unlinkSync(input[3]);
+// // // }
+// // // else {
+// // //     console.log('invalid input');
+// // // }
 
 // // const fs = require('fs');
-// // const input = process.argv;
+// // const path = require('path')
+// // // console.log(path);
+// // const dirPath = path.join(__dirname, 'files');
+// // // console.log(dirPath);
 
-// // if (input[2] == 'add') {
-// //     fs.writeFileSync(input[3], input[4]);
-// // }
-// // else if (input[2] == 'remove') {
-// //     fs.unlinkSync(input[3]);
-// // }
-// // else {
-// //     console.log('invalid input');
-// // }
+// // // for (i = 0; i < 5; i++) {
+// // //     fs.writeFileSync(dirPath + `/hello${i}.txt`, 'testing');
+// // // }
+
+// // fs.readdir(dirPath, (err, files) => {
+// //     // console.log(files);
+// //     files.forEach((item) => {
+// //         console.log(item);
+// //         console.log(err);
+// //     })
+// // })
 
 // const fs = require('fs');
-// const path = require('path')
-// // console.log(path);
-// const dirPath = path.join(__dirname, 'files');
-// // console.log(dirPath);
+// const path = require('path');
+// const dirPath = path.join(__dirname, 'crud');
+// const filePath = `${dirPath}/apple.txt`;
 
-// // for (i = 0; i < 5; i++) {
-// //     fs.writeFileSync(dirPath + `/hello${i}.txt`, 'testing');
-// // }
+// // fs.writeFileSync(filePath, 'This is a simple text file');
 
-// fs.readdir(dirPath, (err, files) => {
-//     // console.log(files);
-//     files.forEach((item) => {
-//         console.log(item);
-//         console.log(err);
-//     })
+// // fs.readFile(filePath, 'utf8', (error, item) => {
+// //     console.log(item);
+// // })
+
+// // fs.appendFile(filePath, ' and file name is apple.txt', (error) => {
+// //     if (!error) console.log("file is updated successfully");
+// // })
+
+// // fs.rename(filePath, `${dirPath}/fruit.txt`, (err) => {
+// //     if (!err) console.log('file name is updated successfully')
+// // })
+
+// fs.unlinkSync(`${dirPath}/fruit.txt`);
+
+// let a = 10;
+// let b = 0;
+
+
+// let waitingData = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve(30);
+//     }, 2000);
+// });
+
+// waitingData.then((b) => {
+//     console.log(a + b);
 // })
 
-const fs = require('fs');
-const path = require('path');
-const dirPath = path.join(__dirname, 'crud');
-const filePath = `${dirPath}/apple.txt`;
+console.log('starting');
 
-// fs.writeFileSync(filePath, 'This is a simple text file');
+setTimeout(() => {
+    console.log('executting after 2 seconds')
+}, 2000);
 
-// fs.readFile(filePath, 'utf8', (error, item) => {
-//     console.log(item);
-// })
+setTimeout(() => {
+    console.log('executting after 0 seconds')
+}, 0);
 
-// fs.appendFile(filePath, ' and file name is apple.txt', (error) => {
-//     if (!error) console.log("file is updated successfully");
-// })
+console.log('finished');
 
-// fs.rename(filePath, `${dirPath}/fruit.txt`, (err) => {
-//     if (!err) console.log('file name is updated successfully')
-// })
 
-fs.unlinkSync(`${dirPath}/fruit.txt`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
