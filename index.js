70cbd9cda231fc676632de865ac80c50ce8abd1f
Kartikey <kartikey.srivastava1271@gmail.com>
2023-05-26 06:25:17 +0530

    template engine

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-const exp = require("constants");
 const express = require("express");
 const path = require("path");
 const app = express();
 
 const publicPath = path.join(__dirname, "public");
 
-app.get("", (req, res) => {
-  res.sendFile(`${publicPath}/index.html`);
-});
+app.set("view engine", "ejs");
 
-app.get("/aboutme", (req, res) => {
-  res.sendFile(`${publicPath}/about.html`);
-});
-
-app.get("*", (req, res) => {
-  res.sendFile(`${publicPath}/invalidpage.html`);
+app.get("/profile", (req, res) => {
+  const user = {
+    name: "Kartikey",
+    email: "kartikey@gmail.com",
+    city: "noida",
+  };
+  res.render("profile", { user });
 });
 
 app.listen(5000);
